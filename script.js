1bf08b12e3087cdf0188f6a6aa74bde40172dfad
Sheng <s.tsui@outlook.com>
2021-03-05 19:00:59 +1300

    updated

--- script.js
+++ script.js
@@ -1,17 +1,46 @@
 const gameBoard = (() => {
     const board = Array(9).fill("");
+    const getBoard = () => board;
 
-    // return {board}???
+    const makeMove = (player, position) => {
+        if (board[position] === "") {
+            board[position] = player.getMarker();
+        return true;
+        } else {
+            return false
+        }
+    };
+
+    const resetBoard = () => {
+        for (let i = 0; i < board.length; i++) {
+            board[i] = "";
+        }
+    }
+
+    const winCondition = [
+        [0,1,2],
+        [3,4,5],
+        [6,7,8],
+        [0,3,6],
+        [1,4,7],
+        [2,5,8],
+        [0,4,8],
+        [2,4,6],
+    ];
+
+    return {getBoard, makeMove, resetBoard};
 })();
 
-const player = (name, marker) => {
+
+const playerFactory = (name, marker) => {
     const getName = () => name;
     const getMarker = () => marker;
+    return {getName, getMarker};
 
-    return {getName, getMarker}
-}
+
+};
 
 const displayController = (() => {
-    const  boardContainer = document.querySelector(".board-container");
-
+    const boardContainer = document.querySelector(".board-container");
+    
 })();
