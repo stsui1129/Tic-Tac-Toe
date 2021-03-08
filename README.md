b558b05c0cf05e06039ff63122e11bc711094aa6
Sheng <s.tsui@outlook.com>
2021-03-08 20:52:15 +1300

    fixed bug, added comments

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 Tic Tac Toe
 ---
-In this project I created a basic tic tac toe game incorporating factory functions and module pattern. This was the most difficult project so far and I had great difficulty of writing code freely since global variables were not allowed.
+In this project I created a basic tic tac toe game incorporating factory functions and module pattern. This was the most difficult project so far and I had great difficulty of writing code freely without the usage of global variables.
 
-There is currently a small bug where the game results in a tie when player 1 makes a winning move on the last turn.
+There was a small bug where making a winning move on the last turn resulted in a tie. This was fixed by switching the order of checkTie()/checkWin() since the tie check overrode the win condition.
 
 https://stsui1129.github.io/Tic-Tac-Toe/
--- script.js
+++ script.js
@@ -20,7 +20,7 @@ const gameBoard = (() => {
             [2,4,6],
         ];
 
-        winningCombinations.forEach((item) => {
+        winningCombinations.forEach((item) => { //for every combination, if square not empty, check to see if position in board are same
             if (board[item[0]] !== "" && board[item[0]] === board[item[1]] && board[item[0]] === board[item[2]]) {
                 displayController.declareWinner();
             }   
@@ -45,22 +45,22 @@ const displayController = (() => {
     turnStatus.textContent = "Player 1 (X)'s turn";
 
     const replay = document.querySelector('#replay-button');
-    replay.addEventListener('click', () => location.reload());
+    replay.addEventListener('click', () => location.reload()); //refreshes page
 
     const placeMarker = (e) => {
         const square = e.target;
         const currentMarker = currentPlayer.getMarker();
-        square.innerHTML = currentMarker;
-        gameBoard.board[square.getAttribute('data-num')] = currentMarker;
-        gameBoard.checkWin();
+        square.innerHTML = currentMarker; //put marks on the physical gameboard
+        gameBoard.board[square.getAttribute('data-num')] = currentMarker; //data-num attributes(0,1,2,...8), puts marker in board array
         gameBoard.checkTie();
+        gameBoard.checkWin(); //tie & win order matters
         toggleTurn();
         turnStatus.textContent = `${currentPlayer.getName()} (${currentPlayer.getMarker()})'s turn`;
     }
 
     const squares = document.querySelectorAll('[data-num]');
     squares.forEach(square => {
-        square.addEventListener('click', placeMarker, { once : true })
+        square.addEventListener('click', placeMarker, { once : true }) //listen should be invoked at most once
     })
     
     const toggleTurn = () => {
@@ -71,7 +71,7 @@ const displayController = (() => {
         turnStatus.remove();
         gameStatus.textContent = `${currentPlayer.getName()} (${currentPlayer.getMarker()}) wins`;
         squares.forEach(square => {
-            square.style.pointerEvents = 'none';
+            square.style.pointerEvents = 'none'; //stops square from reacting to pointer events ('click')
         })
     }
 
